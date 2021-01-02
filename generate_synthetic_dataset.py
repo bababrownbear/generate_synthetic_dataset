6bad95af7d01a18b489fc61567843501c99a6c87
Haseeb Mohammed <haseebmohammed@Haseebs-MBP.cable.rcn.com>
2021-01-01 23:42:11 -0600

    tmp for share.streamlit

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -229,13 +229,13 @@ if __name__ == '__main__':
     # st.markdown(get_table_download_link(dataframe, dataset_name), unsafe_allow_html=True)
 
     STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
-    DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
+    DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "tmp")
     if not DOWNLOADS_PATH.is_dir():
         DOWNLOADS_PATH.mkdir()
 
     dt = datetime.now()
     ms = str(dt.microsecond)
-    st.markdown(f"Download from [downloads/{dataset_name}.csv](downloads/{dataset_name}_{ms}.csv)")
+    st.markdown(f"Download from [tmp/{dataset_name}.csv](tmp/{dataset_name}_{ms}.csv)")
     dataframe.to_csv(str(DOWNLOADS_PATH / f"{dataset_name}_{ms}.csv"), index=False)
 
     synthetic_data_model = SyntheticDataModel(num_samples=num_samples, 
