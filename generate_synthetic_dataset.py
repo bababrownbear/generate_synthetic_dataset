942d1556194848a165c2cff545428879c290bc3a
Haseeb Mohammed <haseebmohammed@Haseebs-MBP.cable.rcn.com>
2021-01-01 18:17:57 -0600

    Added feature weights for negative class.

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -3,16 +3,19 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import base64
 
 
 def generate_synthetic_dataset(n_samples=10, 
                     n_classes=2, 
                     class_weights=[0.5, 0.5], 
                     n_features=1, 
-                    feature_names=['EatsVeggies'], 
-                    class_names=['Yes','No'], 
+                    feature_names=['EatsVeggies'],
+                    feature_types=['Boolean'],
+                    class_names=[True,False], 
                     y_column_name="IsAmazing",
-                    feature_weights=[0.7]):
+                    feature_positive_class_ratio=[0.7],
+                    feature_negative_class_ratio=[0.1]):
 
     if n_samples is None:
         raise ValueError("n_samples cannot be None")
@@ -32,13 +35,20 @@ def generate_synthetic_dataset(n_samples=10,
         raise ValueError("feature_names cannot be None")
     if len(feature_names) != n_features:
         raise ValueError("n_features must equal length of feature_names")
-    if feature_weights is None:
-        raise ValueError("feature_weights cannot be None")
-    if len(feature_weights) != n_features:
-        raise ValueError("n_features must equal length of feature_weights")
-    feature_weights_array = np.asarray(feature_weights)
-    if (feature_weights_array > 1.0).sum() > 0:
-        raise ValueError('feature_weight cannot be greater than 1.0')
+    if feature_positive_class_ratio is None:
+        raise ValueError("feature_positive_class_ratio cannot be None")
+    if len(feature_positive_class_ratio) != n_features:
+        raise ValueError("n_features must equal length of feature_positive_class_ratio")
+    feature_positive_class_ratio_array = np.asarray(feature_positive_class_ratio)
+    if (feature_positive_class_ratio_array > 1.0).sum() > 0:
+        raise ValueError('feature_class_ratio cannot be greater than 1.0')
+    if feature_negative_class_ratio is None:
+        raise ValueError("feature_negative_class_ratio cannot be None")
+    if len(feature_negative_class_ratio) != n_features:
+        raise ValueError("n_features must equal length of feature_negative_class_ratio")
+    feature_negative_class_ratio_array = np.asarray(feature_negative_class_ratio)
+    if (feature_negative_class_ratio_array > 1.0).sum() > 0:
+        raise ValueError('feature_class_ratio cannot be greater than 1.0')
 
 
     X = np.zeros(shape=(n_samples, n_features))
@@ -56,51 +66,80 @@ def generate_synthetic_dataset(n_samples=10,
         start = stop
 
     for ndx, col in enumerate(X.columns,0):
-        col_weight = feature_weights[ndx]
-        n_samples_for_class = int(n_samples * class_weights[0])
-        num_positive_towards_class = int(n_samples_for_class * col_weight)
-        X[X.columns[ndx]][0:num_positive_towards_class] = 'Yes'
-        X[X.columns[ndx]][num_positive_towards_class:] = 'No'
+        col_positive_weight = feature_positive_class_ratio[ndx]
+        col_negative_weight = feature_negative_class_ratio[ndx]
+        n_samples_for_positive_class = int(n_samples * class_weights[0])
+        num_positive_towards_positive_class = int(n_samples_for_positive_class * col_positive_weight)
+        num_positive_towards_negative_class = int(n_samples_for_positive_class * col_negative_weight)
+        if feature_types[ndx] == "Boolean":
+            X[X.columns[ndx]][0:num_positive_towards_positive_class] = True
+            X[X.columns[ndx]][num_positive_towards_positive_class:n_samples_for_positive_class+num_positive_towards_positive_class] = False
+
+            X[X.columns[ndx]][n_samples_for_positive_class:n_samples_for_positive_class+num_positive_towards_negative_class] = True
+            X[X.columns[ndx]][n_samples_for_positive_class+num_positive_towards_negative_class:] = False
 
     dataset = pd.DataFrame(data=pd.concat([X, y], axis=1))
 
     return dataset
 
+def get_table_download_link(df,datasetname):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(
+        csv.encode()
+    ).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="{datasetname}.csv">Download dataset as CSV file</a>'
 
 if __name__ == '__main__':
+    dataset_name = st.sidebar.text_input('Enter dataset name', value='WhoIsAmazing')
+    
     n_samples = st.sidebar.slider(str.format('Number of samples'), 10, 100, step=10)
 
-    class_weight = st.sidebar.slider('Ratio of classes', 0.0, 1.0, step=0.1, value=0.8)
+    class_weight = st.sidebar.slider('Ratio of classes', 0.0, 1.0, step=0.1, value=0.5)
 
     st.sidebar.text_input('Enter column name for y', value='IsAmazing')
 
     n_classes = 2
 
-    # class_names = []
+    class_names = []
 
-    # class_name = st.sidebar.text_input("Enter class name for class #1",value="Yes")
-    # class_names.append(class_name)
+    class_name = st.sidebar.text_input("Enter name for the positive class",value="True")
+    class_names.append(class_name)
 
-    # class_name = st.sidebar.text_input("Enter class name for class #1",value="No")
-    # class_names.append(class_name)
+    class_name = st.sidebar.text_input("Enter name for the negative class",value="False")
+    class_names.append(class_name)
     
     n_features = st.sidebar.slider('Number of features', 1, 10, step=1)
 
     feature_names = []
-    feature_weights = []
+    feature_types = []
+    feature_positive_class_ratio = []
+    feature_negative_class_ratio = []
     for feature_ndx in range(n_features):
         feature_name = st.sidebar.text_input("Enter feature name for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), value="EatsVeggies")
         feature_names.append(feature_name)
-        feature_weight = st.sidebar.slider("Enter weight for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.1, value=1.0)
-        feature_weights.append(feature_weight)
+        feature_type = st.sidebar.selectbox("Select type for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), options=['Boolean'])
+        feature_types.append(feature_type)
+        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs positive class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.1, value=0.5)
+        feature_positive_class_ratio.append(feature_class_ratio)
+        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs negative class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.1, value=0.5)
+        feature_negative_class_ratio.append(feature_class_ratio)
 
     dataframe = generate_synthetic_dataset(n_samples=n_samples, 
                                  n_classes=2, 
                                  class_weights=[class_weight, round(1.0-class_weight,1)], 
-                                #  class_names=class_names,
+                                 class_names=class_names,
                                  n_features=n_features, 
                                  feature_names=feature_names,
-                                 feature_weights=feature_weights
+                                 feature_types=feature_types,
+                                 feature_positive_class_ratio=feature_positive_class_ratio,
+                                 feature_negative_class_ratio=feature_negative_class_ratio
                                  )
 
     st.dataframe(dataframe, width=1000, height=1000)
+
+    st.markdown(get_table_download_link(dataframe, dataset_name), unsafe_allow_html=True)
+
