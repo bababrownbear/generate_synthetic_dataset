b57d0b5430cc70775a005431168729e75c81951a
Haseeb Mohammed <haseebmohammed@Haseebs-MBP.cable.rcn.com>
2021-01-01 18:58:13 -0600

    Added number type, number range.

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -4,18 +4,19 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import base64
+import random
 
-
-def generate_synthetic_dataset(n_samples=10, 
+def generate_synthetic_dataset(n_samples=100, 
                     n_classes=2, 
                     class_weights=[0.5, 0.5], 
-                    n_features=1, 
-                    feature_names=['EatsVeggies'],
-                    feature_types=['Boolean'],
+                    n_features=2, 
+                    feature_names=['EatsVeggies','MathTestScore'],
+                    feature_types=['Boolean','Number'],
+                    feature_number_range=[[],[75,100,25,75]],
                     class_names=[True,False], 
                     y_column_name="IsAmazing",
-                    feature_positive_class_ratio=[0.7],
-                    feature_negative_class_ratio=[0.1]):
+                    feature_positive_class_ratio=[0.7,0.8],
+                    feature_negative_class_ratio=[0.1,0.1]):
 
     if n_samples is None:
         raise ValueError("n_samples cannot be None")
@@ -73,10 +74,17 @@ def generate_synthetic_dataset(n_samples=10,
         num_positive_towards_negative_class = int(n_samples_for_positive_class * col_negative_weight)
         if feature_types[ndx] == "Boolean":
             X[X.columns[ndx]][0:num_positive_towards_positive_class] = True
-            X[X.columns[ndx]][num_positive_towards_positive_class:n_samples_for_positive_class+num_positive_towards_positive_class] = False
+            X[X.columns[ndx]][num_positive_towards_positive_class:n_samples_for_positive_class] = False
 
             X[X.columns[ndx]][n_samples_for_positive_class:n_samples_for_positive_class+num_positive_towards_negative_class] = True
             X[X.columns[ndx]][n_samples_for_positive_class+num_positive_towards_negative_class:] = False
+        elif feature_types[ndx] == 'Number':
+            min_number_range_positive_class,max_number_range_positive_class,min_number_range_negative_class,max_number_range_negative_class = feature_number_range[ndx]
+            X[X.columns[ndx]][0:num_positive_towards_positive_class] = [random.randint(min_number_range_positive_class,max_number_range_positive_class) for _ in range(num_positive_towards_positive_class)]
+            X[X.columns[ndx]][num_positive_towards_positive_class:n_samples_for_positive_class] = [random.randint(min_number_range_negative_class,max_number_range_negative_class) for _ in range(n_samples_for_positive_class-num_positive_towards_positive_class)]
+
+            X[X.columns[ndx]][n_samples_for_positive_class:n_samples_for_positive_class+num_positive_towards_negative_class] = [random.randint(min_number_range_positive_class,max_number_range_positive_class) for _ in range(num_positive_towards_negative_class)]
+            X[X.columns[ndx]][n_samples_for_positive_class+num_positive_towards_negative_class:] = [random.randint(min_number_range_negative_class,max_number_range_negative_class) for _ in range(n_samples-n_samples_for_positive_class-num_positive_towards_negative_class)]
 
     dataset = pd.DataFrame(data=pd.concat([X, y], axis=1))
 
@@ -96,9 +104,7 @@ def get_table_download_link(df,datasetname):
 if __name__ == '__main__':
     dataset_name = st.sidebar.text_input('Enter dataset name', value='WhoIsAmazing')
     
-    n_samples = st.sidebar.slider(str.format('Number of samples'), 10, 100, step=10)
-
-    class_weight = st.sidebar.slider('Ratio of classes', 0.0, 1.0, step=0.1, value=0.5)
+    n_samples = st.sidebar.slider(str.format('Number of samples'), 10, 10000, step=1000)
 
     st.sidebar.text_input('Enter column name for y', value='IsAmazing')
 
@@ -111,21 +117,33 @@ if __name__ == '__main__':
 
     class_name = st.sidebar.text_input("Enter name for the negative class",value="False")
     class_names.append(class_name)
+
+    class_weight = st.sidebar.slider('Ratio of positive to negative classes', 0.0, 1.0, step=0.01, value=0.5)
     
-    n_features = st.sidebar.slider('Number of features', 1, 10, step=1)
+    n_features = st.sidebar.slider('Number of features', 1, 100, step=1)
 
     feature_names = []
     feature_types = []
+    feature_number_range = []
     feature_positive_class_ratio = []
     feature_negative_class_ratio = []
     for feature_ndx in range(n_features):
+        st.sidebar.text("Feature #{feature_ndx}".format(feature_ndx=feature_ndx+1))
         feature_name = st.sidebar.text_input("Enter feature name for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), value="EatsVeggies")
         feature_names.append(feature_name)
-        feature_type = st.sidebar.selectbox("Select type for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), options=['Boolean'])
+        feature_type = st.sidebar.selectbox("Select type for feature #{feature_ndx}".format(feature_ndx=feature_ndx+1), options=['Boolean', 'Number'], index=0)
+        if feature_type == 'Number':
+            min_number_range_positive_class = st.sidebar.number_input("Enter min number range for positive class:",value=0)
+            max_number_range_positive_class = st.sidebar.number_input("Enter max number range for positive class:",value=0)
+            min_number_range_negative_class = st.sidebar.number_input("Enter min number range for negative class:",value=0)
+            max_number_range_negative_class = st.sidebar.number_input("Enter max number range for negative class:",value=0)
+            feature_number_range.append([min_number_range_positive_class,max_number_range_positive_class,min_number_range_negative_class,max_number_range_negative_class])
+        else:
+            feature_number_range .append([])
         feature_types.append(feature_type)
-        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs positive class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.1, value=0.5)
+        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs positive class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.01, value=0.85)
         feature_positive_class_ratio.append(feature_class_ratio)
-        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs negative class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.1, value=0.5)
+        feature_class_ratio = st.sidebar.slider("Enter positive ratio for feature #{feature_ndx} vs negative class".format(feature_ndx=feature_ndx+1), 0.0, 1.0, step=0.01, value=0.05)
         feature_negative_class_ratio.append(feature_class_ratio)
 
     dataframe = generate_synthetic_dataset(n_samples=n_samples, 
@@ -135,9 +153,12 @@ if __name__ == '__main__':
                                  n_features=n_features, 
                                  feature_names=feature_names,
                                  feature_types=feature_types,
+                                 feature_number_range=feature_number_range,
                                  feature_positive_class_ratio=feature_positive_class_ratio,
                                  feature_negative_class_ratio=feature_negative_class_ratio
                                  )
+
+    dataframe = generate_synthetic_dataset()
 
     st.dataframe(dataframe, width=1000, height=1000)
 
